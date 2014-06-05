3deb3443cd966a1f161d6249327b93f7a9347a13
Quynh Nguyen <quynh.xq@gmail.com>
2014-06-05 11:27:44 +0200

    Find top keywords

---
+++ src/TopKeyword.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+/**
+ * Created by quynh on 6/5/14.
+ */
+public class TopKeyword {
+    private String tweets;
+    private HashMap<String, Integer> hashMap;
+
+    public TopKeyword(String tweets)
+    {
+        tweets = tweets;
+        hashMap = new HashMap<String, Integer>();
+
+        // tokenize
+
+        findTopKeyword();
+    }
+
+    public
+
+
+}
